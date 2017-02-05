1abd90649dcdc7210951b0e91e749f817dff646c
Emma Wilcox <icecubesky@gmail.com>
2017-02-04 19:43:06 -0500

    style

--- react/src/components/Trip.js
+++ react/src/components/Trip.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 const Trip = (props) => {
   return(
-    <div>
-      <div onClick={props.handleSelectTrip}>{props.name}</div>
-      <button onClick={props.handleDeleteTrip}>Delete</button>
+    <div className="small-5 small-centered columns">
+      <h6 id="select-name">{props.name}</h6>
+      <div className="row">
+        <button className="react-button" id="select-button" onClick={props.handleSelectTrip}>Select</button>
+        <button className="react-button" id="delete-button" onClick={props.handleDeleteTrip}>Delete</button>
+      </div>
     </div>
   )
 }
