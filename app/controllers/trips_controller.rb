81c243bedcc2ae5840907f08b805b827c46b3cef
Emma Wilcox <icecubesky@gmail.com>
2017-02-05 11:22:17 -0500

    changed when user must be signed in

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,5 +1,6 @@
 class TripsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!
+
   def index
     @trips = Trip.where(user_id: current_user)
   end
