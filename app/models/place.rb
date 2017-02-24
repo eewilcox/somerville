ced21593a5563af5f5294bea766068cfaf9c8dd8
Emma Wilcox <icecubesky@gmail.com>
2017-02-24 09:13:01 -0500

    hound

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,17 +1,16 @@
 class Place
-
   def self.get_info(activity)
     key = ENV["API_KEY"]
-    place = HTTParty.get("https://maps.googleapis.com/maps/api/place/details/json?placeid=#{activity}&key=#{key}")
+    HTTParty.get("https://maps.googleapis.com/maps/api/place/details/json?placeid=#{activity}&key=#{key}")
   end
 
   def self.get_map(map)
     key = ENV["MAP"]
-    place_map = "https://maps.googleapis.com/maps/api/staticmap?center=#{map}&zoom=13&size=200x150&scale=2&markers=size:small|color:0xA7C5BD|#{map}&key=#{key}"
+    "https://maps.googleapis.com/maps/api/staticmap?center=#{map}&zoom=13&size=200x150&scale=2&markers=size:small|color:0xA7C5BD|#{map}&key=#{key}"
   end
 
   def self.get_pic(act_pic)
     key = ENV["API_KEY"]
-    pic = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=1300&photoreference=#{act_pic}&key=#{key}"
+    "https://maps.googleapis.com/maps/api/place/photo?maxwidth=1300&photoreference=#{act_pic}&key=#{key}"
   end
 end
